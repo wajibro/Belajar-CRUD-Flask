3785f47feb01e25b2556aece6946db218a95e62e
Upi PC <wajibro@yahoo.com>
2026-08-23 12:49:11 +0700

    fix

--- app/routes/main.py
+++ app/routes/main.py
@@ -19,7 +19,7 @@ def index():
             }).execute()
         return redirect(url_for('main.index'))
 
-    respons = supabase.table('user_data').select('*').order('id', desc=False)
+    respons = supabase.table('user_data').select('*').order('id', desc=False).execute()
     return render_template('main.html', data_pengguna=respons.data, pengguna_edit=None)
 
 @main_bp.route('/edit/<int:id>', methods=['GET'])
